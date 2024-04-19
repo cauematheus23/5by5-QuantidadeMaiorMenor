dcc8960781b00d552ef93db1f262b38d4fdb788a
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-19 15:44:19 -0300

    Add project files.

---
+++ ExecicioQuantidadeMaiorMenor/Program.cs
@@ -0,0 +1,21 @@
+﻿int idades, cmaior = 0, cmenor = 0, c = 0;
+
+for (; c < 10; c++)
+{
+    do
+    {
+        Console.WriteLine("Digite a idade: ");
+        idades = int.Parse(Console.ReadLine());
+    } while (idades <= 0);
+    if (idades >= 18)
+    {
+        cmaior++;
+
+    }
+    else
+    {
+        cmenor++;
+    }
+}
+Console.WriteLine($"Possuem {cmaior} maiores de idade e {cmenor} menores de idade");
+
